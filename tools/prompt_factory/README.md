2116b4f3ddc9ba4e25594806467389242830b7c2
KMnO4-zx <1021385881@qq.com>
2023-12-15 22:11:10 +0800

    update web_demo

--- tools/prompt_factory/README.md
+++ tools/prompt_factory/README.md
@@ -1,1 +1,17 @@
-这里存放用于制造prompt的工具
+# demo 运行
+
+在`openai.api_key`处填入你的`api_key`，如下所示：
+
+> 注意一定不要将 `api_key` 上传至github仓库，这样会导致 `api_key` 被销毁。
+
+```python
+openai.api_key = 'your_api_key'
+```
+
+使用以下命令运行web_demo
+
+```shell
+streamlit run tools/prompt_factory/web_demo.py
+```
+
+prompt 需要在 `src\prompt\gpt_prompt\prompt.json` 文件中修改。现在只是在 `prompt.json` 中添加了一个简单的例子，可以在 `web_demo` 中看到效果。大家可以在 `prompt.json` 中添加自己任务的prompt，然后在 `web_demo` 中修改加载项。 
---
+++ tools/prompt_factory/web_demo.py
@@ -0,0 +1,47 @@
+import os, json
+import openai
+import streamlit as st
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+openai.api_key = 'sk-BYmrvYL6M4pK7FAJ2cAfDe381cF04303B053BaC419B41899'
+openai.api_base = 'https://api.zyai.online/v1'
+
+with open("src/prompt/gpt_prompt/prompt.json", "r", encoding="utf-8") as f:
+    system_prompt = json.load(f)["test_system"]
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0, messages=[]):
+    '''
+    prompt: 对应的提示词
+    model: 调用的模型，默认为 gpt-3.5-turbo(ChatGPT)
+    '''
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # 模型输出的温度系数，控制输出的随机程度
+    )
+    # 调用 OpenAI 的 ChatCompletion 接口
+    return response.choices[0].message["content"]
+
+with st.sidebar:
+    st.markdown("## 人情世故-天机")
+    temperature = st.slider("Temperature", 0.0, 1.0, 0.7, step=0.01)
+
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by SociaAI")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "system", "content": system_prompt}]
+
+for msg in st.session_state.messages:
+    if msg["role"] == 'system':
+        continue
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = get_completion(prompt, model="gpt-3.5-turbo", temperature=temperature, messages=st.session_state["messages"])
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.chat_message("assistant").write(response)
