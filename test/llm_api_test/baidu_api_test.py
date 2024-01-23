5ce59aecba0a8933c3b9eb2039956b18b3510cda
hl123-123 <1903536218@qq.com>
2024-01-23 22:50:13 +0800

    fix

--- test/llm_api_test/baidu_api_test.py
+++ test/llm_api_test/baidu_api_test.py
@@ -1,27 +1,22 @@
 import os
-from typing import Optional
 from dotenv import load_dotenv
-load_dotenv()
-from metagpt.logs import logger
-from metagpt.provider.base_gpt_api import BaseGPTAPI
 import erniebot
-import random
-from http import HTTPStatus
+
+load_dotenv()
+
 # 当你看不懂代码的时候看看其它代码然后看看文档。
 erniebot.api_type = "aistudio"
+
 
 def call_with_messages(message):
     # 每次用复制这些
     erniebot.access_token = os.environ["BAIDU_API_KEY"]
     ############
     stream = False
-    
-    messages =[{'role': 'user', 'content': message}]
+
+    messages = [{"role": "user", "content": message}]
     response = erniebot.ChatCompletion.create(
-        model="ernie-4.0",
-        messages=messages,
-        top_p=0.95,  # 改 
-        stream=stream
+        model="ernie-4.0", messages=messages, top_p=0.95, stream=stream  # 改
     )
     if stream:
         result = ""
@@ -33,5 +28,5 @@ def call_with_messages(message):
     print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     call_with_messages("你好")
