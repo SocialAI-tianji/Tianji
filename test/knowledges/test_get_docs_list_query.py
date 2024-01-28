2f6f7ec5d6af1d8c70fdc81d74b9142919333af6
sanbuphy <physicoada@gmail.com>
2024-01-28 15:03:39 +0800

    fix zhipuai query

--- test/knowledges/test_get_docs_list_query.py
+++ test/knowledges/test_get_docs_list_query.py
@@ -5,11 +5,13 @@ load_dotenv()
 KNOWLEDGE_PATH = r"D:\1-wsl\TIANJI\Tianji\tianji\knowledges\04-Wishes\knowledges.txt"
 SAVE_PATH = r"D:\1-wsl\TIANJI\Tianji\temp"
 
-doclist = knowledgetool.get_docs_list_query_zhipu(query_str="春节",loader_file_path=KNOWLEDGE_PATH, \
+# doclist = knowledgetool.get_docs_list_query_zhipu(query_str="春节",loader_file_path=KNOWLEDGE_PATH, \
+#                                   persist_directory = SAVE_PATH,k_num=5)
+
+doclist = knowledgetool.get_docs_list_query_openai(query_str="春节",loader_file_path=KNOWLEDGE_PATH, \
                                   persist_directory = SAVE_PATH,k_num=5)
 
 if doclist is not []:
-    for i in doclist:
-        knowledges = knowledges+i.page_content+"\n"
+    print(doclist)
 else:
     print("doclist is [] !")
