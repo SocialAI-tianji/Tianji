920b37d4fed8febe583e4e817ed603265b3e6793
jujimeizuo <fengzetao.zed@foxmail.com>
2024-01-29 16:03:20 +0800

    fix: modify wendao

--- tianji/agents/metagpt_agents/wendao/action.py
+++ tianji/agents/metagpt_agents/wendao/action.py
@@ -1,19 +1,19 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-# 项目名称：人情世故大模型
-# 项目描述：
 
-import sys
+from typing import Optional, Any
+import json
 
 from metagpt.actions import Action
-import json
-from typing import Optional
+from metagpt.logs import logger
+
 from tianji.utils.json_from import SharedDataSingleton
 from tianji.utils.common_llm_api import LLMApi
 
+
 # 设计思路 给定人设并导入参考聊天话术、历史聊天语料进行聊天。
-class read_and_ana(Action):
+class RecvAndAnalyze(Action):
     PROMPT_TEMPLATE: str = """
     你是一个需求语言分析大师，你需要根据"历史消息记录"中的内容分析出以下要素(注意：没如果没有不要回答)：
     1.分析对话需求(requirement)。用关键词表示。如：请帮我写一段祝福。->写一段祝福
@@ -52,7 +52,7 @@ class read_and_ana(Action):
     只需要回复我JSON内容，不需要markdown格式，不需要回复其他任何内容！
     """
 
-    name: str = "read_and_ana"
+    name: str = "RecvAndAnalyze"
 
     async def run(self, instruction: str):
         case = {
@@ -67,7 +67,7 @@ class read_and_ana(Action):
             "time": "傍晚",
             "hobby": "广场舞",
             "wish": "家庭成员平安",
-            "style": "小红书版"
+            "style": "小红书版",
         }
         case1 = {
             "requirement": "给爸爸送祝福",
@@ -81,28 +81,31 @@ class read_and_ana(Action):
             "time": "晚上",
             "hobby": "摄影",
             "wish": "希望我能学会欣赏艺术的美",
-            "style": "老年人版"
+            "style": "老年人版",
         }
-        case = json.dumps(case)
-        case1 = json.dumps(case1)
-        sharedData = SharedDataSingleton.get_instance()
-        print("instruction", instruction)
 
-        prompt = self.PROMPT_TEMPLATE.format(instruction=sharedData.first_status_user_history, case=case, case1=case1)
-        print("prompt", prompt)
+        case = json.dumps(case)
+        # case1 = json.dumps(case1)
+
+        sharedData = SharedDataSingleton.get_instance()
+
+        prompt = self.PROMPT_TEMPLATE.format(
+            instruction=sharedData.first_status_user_history, case=case
+        )
+
         rsp = await LLMApi()._aask(prompt=prompt, top_p=0.1)
         rsp = rsp.replace("```json", "").replace("```", "")
         # rsp = rsp.strip('json\n').rstrip('')
 
-        print("机器人分析需求：", rsp)
+        logger.info("机器人分析需求：" + rsp)
         sharedData.json_from_data = json.loads(rsp)
         # json_from_data = json.loads(rsp)
         return rsp
 
 
 # 设计思路 根据当前状态和聊天与恋爱相关性等综合打分。给出当前回合的打分情况
-class rerask(Action):
-    sharedData: Optional[SharedDataSingleton] = SharedDataSingleton.get_instance()
+class RaiseQuestion(Action):
+    sharedData: Optional[Any] = SharedDataSingleton.get_instance()
     json_from_data: Optional[dict] = sharedData.json_from_data
 
     # PROMPT_TEMPLATE: str = """
@@ -114,53 +117,52 @@ class rerask(Action):
     # 1.友好，活泼
     # 你只需要回复我你的提问内容，不需要任何其他内容!
     # """
+
     PROMPT_TEMPLATE: str = """
-    你是一个提问大师，你只能从"限定提问的问题"中随机选择一个对我进行提问，每次提问只能问一个问题。
+    你是一个提问大师，你只能从"限定提问的问题"中随机选择一个对我进行提问。
     限定提问的问题```
     {question_list_str}
     ```
     每次提问只能问一个问题。
     """
 
-    name: str = "rerask"
+    name: str = "RaiseQuestion"
 
     async def run(self, instruction: str):
-        sharedData: Optional[SharedDataSingleton] = SharedDataSingleton.get_instance()
-        json_from_data: Optional[dict] = sharedData.json_from_data
+        sharedData = SharedDataSingleton.get_instance()
+        json_from_data = sharedData.json_from_data
         # case = {"requirement": "", "scene": "家庭聚会", "festival": "元旦", "role": "妈妈", "age": "中老年人", "career": "退休中学教师", "state": "", "character": "开朗", "time": "傍晚", "hobby": "园艺", "wish": ""}
         # case = json.dumps(json_from_data)
         # print("case",case)
         check_after_question_list = {
             "requirement": "请告诉我你的需求，比如送祝福。",
-            # "scene": "你准备在什么场景下进行呢？比如家庭聚会，朋友聚会等等。",
+            "scene": "你准备在什么场景下进行呢？比如家庭聚会，朋友聚会等等。",
             "festival": "是在哪个特殊的节日(比如中秋节，春节)吗?",
             "role": "你送祝福的对象是谁呢？",
-            # "age": "你送祝福的对象年龄多大呢？",
-            # "career": "送祝福的对象是做什么职业呢？",
-            # "state": "送祝福的对象最近状态如何呢？比如身体状况，精神状况等等。",
-            # "character": "送祝福的对象他有什么性格特点吗？",
+            "age": "你送祝福的对象年龄多大呢？",
+            "career": "送祝福的对象是做什么职业呢？",
+            "state": "送祝福的对象最近状态如何呢？比如身体状况，精神状况等等。",
+            "character": "送祝福的对象他有什么性格特点吗？",
             "time": "你准备在什么时间送祝福呢？",
-            # "hobby": "送祝福的对象有什么习惯吗？",
-            # "wish": "送祝福的对象有哪些个人愿望吗？",
-            "style": "你期望送祝福的语气是老年风格，小红书风格还是带颜文字可爱风格呢?"
+            "hobby": "送祝福的对象有什么习惯吗？",
+            "wish": "送祝福的对象有哪些个人愿望吗？",
+            "style": "你期望送祝福的语气是老年风格，小红书风格还是带颜文字可爱风格呢?",
         }
         question_list = []
         for key, value in json_from_data.items():
-            if key in check_after_question_list:
-                if json_from_data[key] == "":
-                    question_list.append(check_after_question_list[key])
+            if key in check_after_question_list and value == "":
+                question_list.append(check_after_question_list[key])
 
         question_list_str = "\n".join(question_list)
 
         prompt = self.PROMPT_TEMPLATE.format(question_list_str=question_list_str)
-        print("rerask prompt", prompt)
+
         rsp = await LLMApi()._aask(prompt=prompt, top_p=0.1)
-        print("机器人提问：", rsp)
 
         if question_list == []:
             rsp = "YES|" + str(rsp)
         else:
+            logger.info("机器人提问：" + rsp)
             rsp = "NO|" + str(rsp)
-        print(rsp)
-        return rsp
 
+        return rsp
--- tianji/agents/metagpt_agents/wendao/role.py
+++ tianji/agents/metagpt_agents/wendao/role.py
@@ -1,26 +1,28 @@
 from dotenv import load_dotenv
-load_dotenv()
-import sys
-from metagpt.logs import logger
-from metagpt.roles import Role
-from metagpt.schema import Message
-from .action import read_and_ana,rerask
 
-class wendao(Role):
-    name: str = "wendao"
+load_dotenv()
+
+from metagpt.logs import logger
+from metagpt.roles.role import Role, RoleReactMode
+from metagpt.schema import Message
+from .action import RecvAndAnalyze, RaiseQuestion
+
+
+class WenDao(Role):
+    name: str = "WenDao"
     profile: str = "GetInformation"
-    
-    def __init__(self,**kwargs):
+
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._init_actions([read_and_ana,rerask])
-        self._set_react_mode(react_mode="by_order")
+        self._init_actions([RecvAndAnalyze, RaiseQuestion])
+        self._set_react_mode(react_mode=RoleReactMode.BY_ORDER.value)
 
     async def _act(self) -> Message:
         logger.info(f"{self._setting}: to do {self.rc.todo}({self.rc.todo.name})")
-        
+
         todo = self.rc.todo
 
-        msg = self.get_memories(k=1)[0] # find the most k recent messagesA
+        msg = self.get_memories(k=1)[0]
         result = await todo.run(msg.content)
 
         msg = Message(content=result, role=self.profile, cause_by=type(todo))
@@ -28,8 +30,7 @@ class wendao(Role):
         return msg
 
     async def _act_by_order(self) -> Message:
-        """switch action each time by order defined in _init_actions, i.e. _act (Action1) -> _act (Action2) -> ..."""
         for i in range(len(self.states)):
             self._set_state(i)
             rsp = await self._act()
-        return rsp  # return output from the last action
+        return rsp
