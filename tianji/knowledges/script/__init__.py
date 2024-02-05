62b129e56a7a9c68fbb1b932c895e883623f3ace
KaiZhang1228 <goog@tju.edu.cn>
2024-02-05 22:34:37 +0800

    update: Path regularization

---
+++ tianji/knowledges/script/__init__.py
@@ -0,0 +1,1 @@
+from .config import RQA_ST_Liyi_Chroma_Config
--- tianji/knowledges/script/emb.py
+++ tianji/knowledges/script/emb.py
@@ -1,26 +1,23 @@
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma # 向量数据库
+from langchain.vectorstores import Chroma 
+
+from . import RQA_ST_Liyi_Chroma_Config
 
 if __name__ == "__main__":
-    # 定义持久化路径
-    persist_directory = '/root/datasets/liyi/datasets_emb/chroma'
-    data_directory = '/root/datasets/liyi/datasets/'
+    persist_directory = RQA_ST_Liyi_Chroma_Config.PERSIST_DIRECTORY
+    data_directory = RQA_ST_Liyi_Chroma_Config.ORIGIN_DATA
     loader = DirectoryLoader(data_directory, glob="*.txt", loader_cls=TextLoader)
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=150)
     split_docs = text_splitter.split_documents(loader.load())
-    print(len(split_docs))
-    # print(split_docs[0].page_content)
-    # load embeddings of sentence-transformer
+    
+    
     embeddings = HuggingFaceEmbeddings(model_name="/root/weights/model/sentence-transformer")
-
-    # 加载数据库
     vectordb = Chroma.from_documents(
         documents=split_docs,
         embedding=embeddings,
-        persist_directory=persist_directory  # 允许我们将persist_directory目录保存到磁盘上
+        persist_directory=persist_directory 
     )
-    # 将加载的向量数据库持久化到磁盘上
     vectordb.persist()
