0a80886998e15aa0920e85029fa1d9d709b4da8c
goog <33951338+liuxianyi@users.noreply.github.com>
2024-02-07 18:31:27 +0800

    Update config.py

--- tianji/knowledges/script/config.py
+++ tianji/knowledges/script/config.py
@@ -1,3 +1,4 @@
+from metagpt.const import METAGPT_ROOT as TIANJI_PATH
 class RQA_ST_Liyi_Chroma_Config:
     """
     检索问答增强（RQA）配置文件：
@@ -5,9 +6,9 @@ class RQA_ST_Liyi_Chroma_Config:
     基于Sentence-Transformer词向量模型构建的外挂礼仪（Liyi）知识库。
     """
 
-    # 原始数据位置
+    # 原始数据位置 online 设置为空
     ORIGIN_DATA = ""
     # 持久化数据库位置，例如 Tianji/tianji/knowledges/liyi/chroma
-    PERSIST_DIRECTORY = ""
+    PERSIST_DIRECTORY = TIANJI_PATH + "/Tianji/tianji/knowledges/liyi/chroma"
     # Sentence-Transformer词向量模型权重位置
-    HF_SENTENCE_TRANSFORMER_WEIGHT = ""
+    HF_SENTENCE_TRANSFORMER_WEIGHT = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
