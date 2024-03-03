e78f00cc36f257198c336efff830abed474b3acc
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-03-03 18:23:33 +0800

    solve import and .env issue

--- test/embedding/local/BaseLocal.py
+++ test/embedding/local/BaseLocal.py
@@ -1,12 +1,13 @@
 from typing import List
 
+
 class BaseLocalEmbeddings:
     """
     Base class for local embeddings
     """
+
     def __init__(self, path: str) -> None:
         self.path = path
-    
+
     def get_embedding(self, text: str, model: str) -> List[float]:
         raise NotImplementedError
-    
--- test/embedding/online/BaseOnline.py
+++ test/embedding/online/BaseOnline.py
@@ -1,13 +1,13 @@
 from typing import List
 
+
 class BaseOnlineEmbeddings:
     """
     Base class for online embeddings
     """
+
     def __init__(self) -> None:
         pass
-        
+
     def get_embedding(self, text: str, model: str) -> List[float]:
         raise NotImplementedError
-    
-    
