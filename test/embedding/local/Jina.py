42efde2c4b0af7e2412e6b78e0fc6d3d05a59e54
tackhwa <tackhwawong@hotmail.com>
2024-03-03 17:12:41 +0800

    test2

--- test/embedding/local/Jina.py
+++ test/embedding/local/Jina.py
@@ -1,23 +1,30 @@
 from BaseLocal import BaseLocalEmbeddings
 from typing import List
 import os
-#os.environ["HF_TOKEN"]=""
+import torch
+from transformers import AutoModel
+from dotenv import load_dotenv
+
+# 加载.env文件
+load_dotenv()
+
+# os.environ["HF_TOKEN"]=""
+
 
 class JinaEmbedding(BaseLocalEmbeddings):
     """
     class for Jina embeddings
     """
-    #path:str = TIANJI_PATH / "embedding/jinaai/jina-embeddings-v2-base-zh"
-    def __init__(self, path:str='jinaai/jina-embeddings-v2-base-zh') -> None:
+
+    # path:str = TIANJI_PATH / "embedding/jinaai/jina-embeddings-v2-base-zh"
+    def __init__(self, path: str = "jinaai/jina-embeddings-v2-base-zh") -> None:
         super().__init__(path)
         self._model = self.load_model()
-        
-    def get_embedding(self, text: str)-> List[float]:
+
+    def get_embedding(self, text: str) -> List[float]:
         return self._model.encode([text])[0]
-    
+
     def load_model(self):
-        import torch
-        from transformers import AutoModel
         if torch.cuda.is_available():
             device = torch.device("cuda")
         else:
@@ -25,9 +32,12 @@ class JinaEmbedding(BaseLocalEmbeddings):
         model = AutoModel.from_pretrained(self.path, trust_remote_code=True).to(device)
         return model
 
+
 if __name__ == "__main__":
     Jina = JinaEmbedding()
     embedding_result = Jina.get_embedding("你好")
-    print(f"Result of Jina Embedding: \n"
-      f"\t Type of output: {type(embedding_result)}\n"
-      f"\t Shape of output: {len(embedding_result)}")
+    print(
+        f"Result of Jina Embedding: \n"
+        f"\t Type of output: {type(embedding_result)}\n"
+        f"\t Shape of output: {len(embedding_result)}"
+    )
