50bfabe54ee3c6f88d213cbd895984e6cc6a8243
sanbuphy <physicoada@gmail.com>
2024-07-16 21:43:26 +0800

    fix

--- run/xlab/tianji_etiquette_app.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system(
-    "streamlit run ./run/xlab/tianji_etiquette.py --server.address=0.0.0.0 --server.port 7861"
-)
--- run/xlab/tianji_wish_app.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system(
-    "streamlit run ./run/xlab/tianji_wish.py --server.address=0.0.0.0 --server.port 7860"
-)
